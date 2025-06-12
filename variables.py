634e7a33b49a704987127ddd43e5715ff906ac1e
sebastian <s15601774@gmail.com>
2025-06-11 19:03:55 -0500

    up1

---
+++ variables.py
@@ -0,0 +1,16 @@
+a = 18
+b = 35
+c = a+b
+d = a-b
+e = a / b
+f = a * b
+g = a ** b
+h = a // b
+i = a % b
+print ("el resultado de la suma es", c)
+print ("el resultado de la resta es", d)
+print ("el resultado de la division es", e)
+print ("el resultado de la multiplicacion es", f)
+print ("el resultado de la potencia es", g)
+print ("el resultado de la division entera", h) #division sin decimal
+print ("el resultado del modulo ", i) #por ejemplo si son personas no divides una esta sobra
